6ca780247a89f9f7a4dea7de51b89967df04a6d9
Gabriel Gian <gabrielgian@protonmail.com>
2020-08-02 18:47:37 -0300

    Adicionar funções

--- land_of_lisp/wizards-apprentice.lisp
+++ land_of_lisp/wizards-apprentice.lisp
@@ -13,5 +13,49 @@
                         (garden (living-room east door))
                         (attic (living-room downstairs ladder))))
 
+(defparameter *objects* '(whyskey bucket frog chain)
+
+(defparameter *object-locations* '((whiskey living-room) 
+								   (bucket living-room)
+								   (chain garden)
+								   (frog garden)))
+
+(defparameter *location* 'living-room)
+
 (defun describe-path (edge)
     `(there is a ,(caddr edge) going ,(cadr edge) from here.))
+
+(defun describe-paths (location edges)
+ 	(apply #'append (mapcar #'describe-path (cdr (assoc location-edges)))))
+
+(defun objects-at (loc objs obj-locs)
+  (labels ((at-loc-p (obj) 
+					 (eq (cadr (assoc obj obj-locs)) loc)))
+	(remove-if-not #'at-loc-p objs)))
+
+(defun describe-objects (loc objs obj-loc) 
+  (labels ((describe-obj (obj)
+						 `(you see a ,obj on the floor.)))
+	(apply #'append (mapcar #'describe-obj (objects-at loc objs obj-locs)))))
+
+(defun look () 
+  (append (describe-location *location* *nodes*)
+		  (describe-paths *location* *edges*)
+		  (describe-objects *location* *objects* *object-locations)))
+
+(defun walk (direction) 
+  (let ((next (find direction
+					(cdr (assoc *location* *edges*))
+					:key #'cadr)))
+	(if next
+	  (progn (setf *location* (car next))
+			 (look))
+	  '(you cannot go that way))))
+
+(defun pickup (object) 
+  (cond ((member object
+				 (objects-at *location* *objects* *object-locations*))
+		 (push (list object 'body) *object-locations*)
+		 `(you are now carrying the ,object))
+		 (t '(you cannot get that.))))
+
