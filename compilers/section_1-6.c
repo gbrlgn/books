c2e282fb4e3896a738a2d725e12149267ae4d15c
Gabriel Gian <gabrielgian@protonmail.com>
2021-03-15 20:06:38 -0300

    Answer section 1.6 exercises.

--- compilers/section_1-6.c
+++ compilers/section_1-6.c
@@ -2,45 +2,56 @@ int w, x, y, z;
 int i = 4; int j = 5;
 {   int j = 7;
     i = 6;
-    w = i + j;
+    w = i + j; // w = 13
 }
-x = i + j;
+x = i + j; // x = 11
 { int i = 8;
-    y = i + j;
+    y = i + j; // y = 13
 }
-z = i + j;
+z = i + j; // z = 11
 
 int w, x, y, z;
 int i = 3; int j = 4;
 {   int i = 5;
-    w = i + j;
+    w = i + j; // w = 9
 }
-x = i + j;
-{ int j = 6;
+x = i + j; // x = 7
+{   int j = 6;
     i = 7;
-    y = i + j;
+    y = i + j; // y = 13
 }
-z = i + j;
+z = i + j; // z = 11
 
-//Figure 1.13
+// Figure 1.13
 
 
-{   int w, x, y, z;
-    {   int x, z;
-        {   int w, x;
+{   int w, x, y, z; // W = B1, B3, B4
+                    // X = B1, B2, B4
+                    // Y = B1, B5
+                    // Z = B1, B2, B5
+    {   int x, z; // X = B2, B3
+                  // Z = B2
+        {   int w, x; // W = B3
+                      // X = B3
     }
-    {   int w, x;
-        {   int y, z;
+    {   int w, x; // W = B4
+                  // X = B4
+        {   int y, z; // Y = B5
+                      // Z = B5
     }
 }
 
-//Figure 1.14
+// Figure 1.14
 
 
-#define a (x+1)
+#define a (x + 1)
 int x = 2;
-void b() { x = a; printf("%d\n"); }
-void b() { int x = 1; printf("%d\n"), a; }
+void b() { x = a; printf("%d\n", x); }
+void b() { int x = 1; printf("%d\n", a); }
 void main() { b(); c(); }
 
-//Figure 1.15
+// Output:
+// 3
+// 2
+
+// Figure 1.15
