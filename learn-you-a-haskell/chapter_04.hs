90b233f209ea50c2c7ddd418b7248d9df042a65d
Gabriel Gian <gabrielgian@protonmail.com>
2022-09-16 17:41:45 -0300

    Add exercises from chapter 4

---
+++ learn-you-a-haskell/chapter_04.hs
@@ -0,0 +1,34 @@
+maximum' :: (Ord a) => [a] -> a
+maximum' [] = "Empty list."
+maximum' [x] = x
+maximum' (x:xs) = max x (maximum' xs)
+
+replicate :: Int -> a -> [a]
+replicate n x
+    | n <= 0 []
+    | x : replicate' (n - 1) x
+
+take' :: Int -> [a] -> [a]
+take' n _
+    | n <= 0 = []
+take' _ [] = []
+take' n (x:xs) = x : take' (n - 1) xs
+
+reverse' :: [a] -> [a]
+reverse' x
+    | x == [] = []
+reverse' (x:xs) = reverse' xs ++ [x]
+
+repeat' :: a -> [a]
+repeat' x = x : repeat' x
+
+zip' :: [a] -> [b] -> [(a, b)]
+zip' _ [] = []
+zip' [] _ = []
+zip' (x:xs) (y:ys) = (x, y) : zip' xs ys
+
+elem' :: (Eq a) => a -> [a] -> Bool
+elem' _ [] = False
+elem' a (x:xs)
+    | a == x = True
+    | otherwise a `elem'` xs
