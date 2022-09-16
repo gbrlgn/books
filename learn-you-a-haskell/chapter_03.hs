22489575c7647f34d8b006b8b9c38940f94aeee7
Gabriel Gian <gabrielgian@protonmail.com>
2022-09-16 16:33:57 -0300

    Finish exercises from chapter 3

--- learn-you-a-haskell/chapter_03.hs
+++ learn-you-a-haskell/chapter_03.hs
@@ -76,3 +76,45 @@ bmiTell weight height
           skinny = 18.5
           normal = 25.0
           overweight = 30.0
+    -- where bmi = weight / height ^ 2
+    --       (skinny, normal, fat) = (18.5, 25.0, 30.0)
+
+initials :: String -> String -> String
+initials firstname lastname = [f] ++ ". " ++ [l] ++ "."
+    where (f:_) = firstname
+          (l:_) = lastname
+
+calcBmis :: [(Double, Double)] -> [Double]
+calcBmis xs = [bmi w h | (w, h) <- xs]
+    where bmi weight height = weight / height ^ 2
+
+cylinder :: Double -> Double -> Double
+cylinder r h =
+    let sideArea = 2 * pi * r * h
+        topArea = pi * r ^ 2
+    in sideArea + 2 * topArea
+
+calcBmis :: [(Double, Double)] -> [Double]
+calcBmis xs = [bmi | (w, h) <- xs, let bmi = w / h ^ 2]
+
+calcBmis :: [(Double, Double)] -> [Double]
+calcBmis xs = [bmi | (w, h) <- xs, let bmi = w / h ^ 2, bmi > 25.0]
+
+head' :: [a] -> a
+head' [] = error "No head for empty lists"
+head' (x:_) = x
+
+head' :: [a] -> a
+head' xs = case xs of [] -> error "No head for empty lists"
+                      (x:_) -> x
+
+describeList :: [a] -> String
+describeList ls = "The list is " ++ case ls of [] -> "empty"
+                                               [x] -> "a singleton list"
+                                               xs -> "a longer list"
+
+describeList :: [a] -> String
+describeList ls = "The list is " ++ what ls
+    where what [] = "empty."
+          what [x] = "a singleton list."
+          what xs = "a longer list"
