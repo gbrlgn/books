cc12ffaad19e49064ce917c3c0c7c726f784f05e
Gabriel Gian <gabrielgian@protonmail.com>
2022-10-06 17:32:44 -0300

    Update Cargo.tomls and fix typo

--- rspl/chapter_04/ownership/src/main.rs
+++ rspl/chapter_04/ownership/src/main.rs
@@ -13,7 +13,7 @@ fn main() {
     let word = first_word(my_string_literal);
 }
 
-fn first_word(s: &String) - > &str {
+fn first_word(s: &String) -> &str {
     let bytes = s.as_bytes();
 
     for (i, &item) in bytes.iter().enumerate() {
