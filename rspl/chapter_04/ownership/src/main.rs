95cca94a5ea42d4a4daeff7ed91c90ddfca374d2
Gabriel Gian <gabrielgian@protonmail.com>
2022-10-06 17:29:17 -0300

    Add files from chapter 04

---
+++ rspl/chapter_04/ownership/src/main.rs
@@ -0,0 +1,26 @@
+fn main() {
+    let my_string = String::from("hello world");
+
+    // burrow
+    let word = first_word(&my_string[..]);
+
+    let my_string_literal = "hello world";
+
+    // burrow
+    let word = first_word(&my_string_literal[..]);
+
+    // copy
+    let word = first_word(my_string_literal);
+}
+
+fn first_word(s: &String) - > &str {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[0..i]
+        }
+    }
+
+    &s[..]
+}
